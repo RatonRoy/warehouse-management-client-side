4989f63ef56fa3e89e42aa7e5e4d99f61ac9646b
RatonRoy <royraton18@gmail.com>
2022-05-13 18:29:49 +0600

    complete the stock section and home extra two section

--- src/components/Pages/Home/Stocks/Stocks.js
+++ src/components/Pages/Home/Stocks/Stocks.js
@@ -2,46 +2,60 @@ import React from 'react';
 import './Stocks.css';
 const Stocks = () => {
 	return (
-		
-	<section className = "stocks-wrap">
-		<h2 className = "stock-title"> Our Stocks  </h2>
-		<div className = "section-center stockbar-wrap">
-		
-			<div className = "stock-container">
-				<h4>Mango </h4>
-				<div className = " stockbar-container">
-					<div className = "stock mango">90%</div>
-					{/* <div class="skills html">90%</div> */}
+
+		<section className="stocks-wrap">
+			<h2 className="stock-title"> Our Stocks  </h2>
+			<div className="section-center stockbar-wrap">
+				<div className="stock-container">
+					<h4>Watermelon</h4>
+					<div className="stockbar-container">
+						<div className="stock Watermelon">5%</div>
+					</div>
 				</div>
-			</div>
-			<div className = "stock-container">
-				<h4>Pineapple</h4>
-				<div className = " stockbar-container">
-					<div className = "stock Pineapple">85%</div>
+				{/* end watermelon  */}
+
+				<div className="stock-container">
+					<h4>Pineapple</h4>
+					<div className=" stockbar-container">
+						<div className="stock Pineapple">8%</div>
+					</div>
 				</div>
-			</div>
-			<div className = "stock-container">
-				<h4>Watermelon</h4>
-				<div className = " stockbar-container">
-					<div className = "stock Watermelon">75%</div>
+				<div className="stock-container">
+					<h4>Apple</h4>
+					<div className=" stockbar-container">
+						<div className="stock Apple">12%</div>
+					</div>
 				</div>
-			</div>
-			<div className = "stock-container">
-				<h4>Apple</h4>
-				<div className = " stockbar-container">
-					<div className = "stock Apple">60%</div>
+				<div className="stock-container">
+					<h4> Jackfruit </h4>
+					<div className="stockbar-container">
+						<div className="stock Jackfruit">10%</div>
+					</div>
 				</div>
-			</div>
-			<div className = "stock-container">
-				<h4> Jackfruit </h4>
-				<div className = "stockbar-container">
-					<div className = "stock Jackfruit">40%</div>
+				<div className="stock-container">
+					<h4> Abroad  fruits </h4>
+					<div className="stockbar-container">
+						<div className="stock abroad-fruits">15%</div>
+					</div>
 				</div>
+				{/* end  */}
+
+				<div className="stock-container">
+					<h4>Mango </h4>
+					<div className=" stockbar-container">
+						<div className="stock mango">18%</div>
+					</div>
+				</div>
+				<div className="stock-container">
+					<h4> seasonal fruits </h4>
+					<div className="stockbar-container">
+						<div className="stock seasonal-fruits">32%</div>
+					</div>
+				</div>
+
 			</div>
-		
-		</div>
-	</section>
-	
+		</section>
+
 	);
 };
 
