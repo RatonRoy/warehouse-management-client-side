ac317600fca565454105b1d52bedb95f628ddbdd
Raton Roy <75809073+RatonRoy@users.noreply.github.com>
2022-05-15 15:40:03 +0600

    Update FruitDetails.js

--- src/components/Pages/FruitDetails/FruitDetails.js
+++ src/components/Pages/FruitDetails/FruitDetails.js
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import useInventoryDetail from '../../Hooks/useInventoryDetail';
 import './FruitDetails.css';
 
 
 const FruitDetails = () => {
 	const { InventoryId } = useParams()
-	const [inventory, setInventory] = useInventoryDetail(InventoryId);
+	const [inventory, setInventory] = useState({});
+	useEffect(() => {
+		const url = `https://safe-hamlet-12951.herokuapp.com/allinventory/${InventoryId}`;
+		fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data.name)
+				setInventory(data)
+			})
+	}, [InventoryId])
 
-
-	let { name, img, supplier, price, quantity, description, sold } = inventory;
-
+	let quantity = inventory.quantity;
+	
+	// const [quantityValue, setQuantity] = useState({inventory: });
+	// console.log(quantityValue);
 	// update Info 
 	const handleUpdatefruit = () => {
-		quantity = quantity + 5;
 
-
+		quantity = quantity - 5;
 		const updatedQuantity = { quantity };
-
-
 		// send data to the server
 		const url = `https://safe-hamlet-12951.herokuapp.com/allinventory/${InventoryId}`;
 		fetch(url, {
@@ -32,46 +38,102 @@ const FruitDetails = () => {
 			.then(data => {
 				console.log('success', data, updatedQuantity);
 				// alert('fruit added successfully!!!');
-				// setInventory(data)
+				if (data.modifiedCount > 0) {
+					// setQuantity(updatedQuantity);
+				}
+
+
 
 			})
 		window.location.reload();
 	}
+
+	const handaleFruitAdd = (e) => {
+		e.preventDefault();
+		const addvalue = e.target.add.value;
+		const addNum = parseInt(addvalue);
+		if (addNum >= 5) {
+			quantity = quantity + addNum;
+			const updatedQuantity = { quantity };
+			// send data to the server
+			const url = `https://safe-hamlet-12951.herokuapp.com/allinventory/${InventoryId}`;
+			fetch(url, {
+				method: 'PUT',
+				headers: {
+					'content-type': 'application/json'
+				},
+				body: JSON.stringify(updatedQuantity)
+			})
+				.then(res => res.json())
+				.then(data => {
+					console.log('success', data, updatedQuantity);
+					// alert('fruit added successfully!!!');
+					// setInventory(data)
+
+				})
+			window.location.reload();
+
+		}
+
+	}
 	// end of update info 
 	return (
-		<section className="manage-container">
-			<h1 className='manage-title'> Manage : {name} </h1>
+		<section className='manage-container'>
+			<h1 className='manage-title'> Manage {inventory.name} </h1>
 			<div className="manage-fruit-wrapper">
 				<div className="manage-fruit-img">
-					<img src={img} alt="" />
+					<img src={inventory.img} alt="" />
 				</div>
 				<article className="manage-fruit-info">
 					<div className="manage-fruit-data">
 						<div className="price-quantity">
-							<p> Price :  {price}Taka/kg  </p>
-							<p> Quantity :  {quantity}kg  </p>
+							<p> Price :  {inventory.price}Taka/kg  </p>
+							<p> Quantity :  {inventory.quantity}kg  </p>
 						</div>
 						<div className="suppplier-sold">
-							<p> Supplier : {supplier}</p>
-							<p> Sold : {sold}kg </p>
+							<p> Supplier : {inventory.supplier}</p>
+							<p> Sold : {inventory.sold}kg </p>
 						</div>
 					</div>
 					<div className="description">
 						<p>
-							{description}
+							{inventory.description}
 						</p>
 					</div>
 					<div className="manage-fruit-btn-container">
-						<button className='manage-fruit-btn' onClick={handleUpdatefruit}>
-							Deliver Min 5Kg
-						</button>
-						<Link className='manage-fruit-btn' to={`/checkout/${InventoryId}`}>
-							Place Order
-						</Link>
+						<p>
+							<button className='manage-fruit-btn' onClick={handleUpdatefruit}>
+								Deliver  5Kg
+							</button>
+						</p>
+						<p>
+							<Link className='manage-fruit-btn' to={`/checkout/${InventoryId}`}>
+								Place Order
+							</Link>
+						</p>
 					</div>
+					<form onSubmit={handaleFruitAdd}>
+						<input type="text" placeholder='Add min 5 kg' name='add' className='add-input' />
+						<input type="submit" value='Add Fruit' className='add-btn' />
+					</form>
 				</article>
+
+
+
+
+
+				
 			</div>
 		</section>
+
+
+
+
+
+
+
+
+
 	);
 };
 
